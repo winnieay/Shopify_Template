f27f41cab40bb86d0c5e6851fabc389f624074c8
winnieay <88380759+winnieay@users.noreply.github.com>
2022-12-17 16:38:39 +0800

    Update brands.js

--- assets/brands.js
+++ assets/brands.js
@@ -1,14 +1,12 @@
-console.log("this sfakdfzkjmns");
+
   var button = document.getElementById('next');
   button.onclick = function() {
-    console.log('GO RIGHT');
     var container = document.getElementById('slider');
     sideScroll(container, 'right', 25, 200, 20);
   };
 
   var back = document.getElementById('pre');
   back.onclick = function() {
-    console.log('GO LEFT');
     var container = document.getElementById('slider');
     sideScroll(container, 'left', 25, 200, 20);
   };
@@ -18,10 +16,8 @@ console.log("this sfakdfzkjmns");
     var slideTimer = setInterval(function() {
       if (direction == 'left') {
         element.scrollLeft -= step;
-        console.log('a');
       } else {
         element.scrollLeft += step;
-        console.log('b');
       }
       scrollAmount += step;
       if (scrollAmount >= distance) {
